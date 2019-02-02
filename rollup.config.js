59630720efddca533268c108eba250ae4581b45d
Iiro Jäppinen <iiro@jappinen.fi>
2019-02-02 21:29:36 +0200

    fix: use named exports in Rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
-import commonjs from 'rollup-plugin-commonjs';
+import commonjs from "rollup-plugin-commonjs";
 import minify from "rollup-plugin-babel-minify";
-import resolve from 'rollup-plugin-node-resolve';
+import resolve from "rollup-plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
 
 import pkg from "./package.json";
@@ -10,45 +10,60 @@ const production = !process.env.ROLLUP_WATCH;
 const plugins = [
   typescript({ typescript: require("typescript") }),
   production && minify({ comments: false })
-]
-const umdPlugins = [
-  ...plugins,
-  resolve(),
-  commonjs(),
-]
+];
+
+const umdPlugins = [...plugins, resolve(), commonjs()];
 
 export default [
   {
     input: "index.ts",
     output: [
-      { file: pkg.main, format: "cjs" },
-      { file: pkg.module, format: "es" }
+      {
+        exports: "named",
+        file: pkg.main,
+        format: "cjs"
+      },
+      {
+        exports: "named",
+        file: pkg.module,
+        format: "es"
+      }
     ],
     plugins
   },
   {
     input: "string.ts",
     output: [
-      { file: 'string.js', format: "cjs" },
-      { file: 'string.esm.js', format: "es" }
+      {
+        exports: "named",
+        file: "string.js",
+        format: "cjs"
+      },
+      {
+        exports: "named",
+        file: "string.esm.js",
+        format: "es"
+      }
     ],
     plugins
   },
   {
     input: "index.ts",
     output: {
+      exports: "named",
       file: pkg.browser,
-      format: 'umd',
-      name: 'reactStyleReset'
+      format: "umd",
+      name: "reactStyleReset"
     },
     plugins: umdPlugins
   },
   {
     input: "string.ts",
     output: {
-      file: 'string.umd.js',
-      format: 'umd',
-      name: 'reactStyleReset'
+      exports: "named",
+      file: "string.umd.js",
+      format: "umd",
+      name: "reactStyleReset"
     },
     plugins: umdPlugins
   }
